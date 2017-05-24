097c87b45997ca878f56b6f8cb175d2cd474a42d
caohao <705083979@qq.com>
2017-05-24 13:31:16 +0800

    展示页面

--- src/main/java/app/controller/PageController.java
+++ src/main/java/app/controller/PageController.java
@@ -2,6 +2,7 @@ package app.controller;
 
 import org.springframework.boot.autoconfigure.web.WebMvcProperties;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -11,9 +12,15 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class PageController {
 
-    @RequestMapping(value = "/index")
+    @RequestMapping(value = "/")
     public ModelAndView index(){
         return new ModelAndView("index");
     }
+
+    @RequestMapping(value = "/{page}")
+    public ModelAndView page(@PathVariable String page){
+        return new ModelAndView(page);
+    }
+
 
 }
